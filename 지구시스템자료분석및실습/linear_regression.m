0a2b96f82f8abca024970c2d71174b34e9a23eb2
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-04-29 01:14:28 +0900

    Update linear_regression.m

--- 지구시스템자료분석및실습/linear_regression.m
+++ 지구시스템자료분석및실습/linear_regression.m
@@ -1,19 +1,29 @@
 data = load('agedepth_1.txt');
-depth = data(:,1);
-age = data(:,2);
+x = data(:,1);
+y = data(:,2);
+
 % 1차식으로 regression
 % y = a1x + a0
-p = polyfit(depth,age,1);
+
+N = length(x);
+temp = [x,ones(N,1)];
+% p = polyfit(depth,age,1); 
+p = (temp'*temp)\(temp'*y);
 
 % regression 나타내기
 xx = [0:0.5:20]; % min(x)와 max(x)로 범위를 판단한다.
-yy = polyval(p, xx);
-figure, plot(depth, age,'*');
+% yy = polyval(p, xx); 
+yy = p(2) + p(1) * xx;
+figure, plot(x, y,'*');
 hold on, plot(xx,yy,'-r');
 
 % 95% 이내의 값들을 알아보자
-[p, S] = polyfit(depth, age, 1);
+[p, S] = polyfit(x, y, 1);
 [yy, delta] = polyval(p,xx,S);
 hold on, plot(xx,yy+delta,'--k'), plot(xx,yy-delta,'--k');
 
-% 04/12 12분부터...
+% residual을 구하기
+xhat = x;
+yhat = p(1) * xhat + p(2);
+residual = y - yhat;
+RMSE = sqrt(sum((residual).^2) / N);
