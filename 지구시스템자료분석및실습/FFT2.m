4627506d2a43f1dd7ce87d6d82c9097bb2d93c3f
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-05-10 19:01:20 +0900

    Update FFT2.m

--- 지구시스템자료분석및실습/FFT2.m
+++ 지구시스템자료분석및실습/FFT2.m
@@ -20,7 +20,7 @@ clear all;
 % rectangle function (box car)
 y = zeros(64,1);
 y(33-16:33+16) = 1;
-figure,plot(y);
+% figure,plot(y);
 fy = fft(y);
 fy = fftshift(fy);
 % figure,plot(real(fy));
@@ -29,11 +29,31 @@ fy = fftshift(fy);
 dt = 1;
 n = length(fy);
 f = [0:n-1]'/n - 0.5;
-figure,plot(f, real(fy))
+% figure,plot(f, real(fy))
 
 % inverse Fourier transform for sinc function
 clear y;
 y = ifft(ifftshift(fy));
-figure,plot(real(y));
+% figure,plot(real(y));
 
-% 27min....
+% before time shift
+y = zeros(64,1);
+y(1) = 1;
+figure, stem(y)
+fy = fftshift(fft(y));  % FFT(y)
+figure, stem(real(fy));
+
+% after time shift
+y(1) = 0;
+y(10) = 1;
+fy = fftshift(fft(y));  % FFT(y), but time shifted
+figure, stem(real(fy));
+
+% To recover from shifting, calculate exp(-iwt0)
+N = length(fy);
+f = [0:N-1]'/N - 0.5;   % normalize
+t0 = 9;
+fs = exp(-j * 2 * pi * f * t0);
+ff = fy.*conj(fs);  % multiply conjugate complex
+z = ifft(ifftshift(ff));
+figure, stem(real(z));
